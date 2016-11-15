4a27e24249e87214fee77690f0b7f02d612f058a
Alexander Ferreras <ferrerasalexander@gmail.com>
2016-11-15 13:27:40 -0400

    primer cambio

---
+++ tests/codeception/config/config.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Application configuration shared by all test types
+ */
+return [
+    'controllerMap' => [
+        'fixture' => [
+            'class' => 'yii\faker\FixtureController',
+            'fixtureDataPath' => '@tests/codeception/fixtures',
+            'templatePath' => '@tests/codeception/templates',
+            'namespace' => 'tests\codeception\fixtures',
+        ],
+    ],
+    'components' => [
+        'db' => [
+            'dsn' => 'mysql:host=localhost;dbname=yii2_basic_tests',
+        ],
+        'mailer' => [
+            'useFileTransport' => true,
+        ],
+        'urlManager' => [
+            'urlFormat'=>'path',
+            'showScriptName' => false,
+              'rules'=>[
+                                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                                '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+                    ],
+        ],
+    ],
+];
